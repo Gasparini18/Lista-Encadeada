2a5eae022d77530ea02b61417ed571630baeab3e
Gasparini18 <112758450+Gasparini18@users.noreply.github.com>
2023-03-09 11:24:57 -0300

    Create No.java

---
+++ No.java
@@ -0,0 +1,16 @@
+
+package listaencadeada;
+
+
+public class No {
+    String nome;
+    String tel;
+    No proximo;
+    
+    No(String nome, String tel){
+        this.nome = nome;
+        this.tel = tel;
+        proximo = null;
+    
+    }
+}
